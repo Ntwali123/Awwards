a81568251e2267b0d754bcbe5a365e4e4641f571
nshutiyvonne1@gmail.com <ntwalidavid90@gmail.com>
2025-01-05 19:52:26 +0200

    made about us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Hero from "./components/Hero";
-
+// import Hero from "./components/Hero";
+import About from "./components/About";
 const App = () => {
   return (
     <main className="relative max-h-screen w-screen overflow-x-hidden">
-      <Hero />
+      {/* <Hero /> */}
+      <About />
     </main>
   );
 };
